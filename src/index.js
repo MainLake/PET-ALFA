1d186f3117723ad1e408c380d6db338cd2a602a1
Pablo Julian Garay de Leon <leonmaing@gmail.com>
2023-10-18 17:00:28 -0600

    Login terminado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import { UserContextProvider } from "./context/contextUser/ContextUser";
+import Initializer from "./routes/initializer/Initializer";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <UserContextProvider>
+    <Initializer>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Initializer>
+  </UserContextProvider>,
+  document.getElementById("root")
 );
---
+++ src/routes/initializer/Initializer.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useUserContext } from "../../context/contextUser/ContextUser";
+
+
+const Initializer = ({children}) => {
+    const [user, setUser] = useUserContext();
+
+    console.log('Dentro de inicializador')
+
+    useEffect(() => {
+
+        const userObject = JSON.parse(window.localStorage.getItem('userPET'));
+
+        if(userObject !== null || userObject !== undefined) {
+            setUser(userObject);
+        }
+
+    }, [setUser])
+
+    return children;    
+}
+
+export default Initializer;
