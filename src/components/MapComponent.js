466882595051b6af34b19455c7e617d26dd3ee36
Jeac04 <jeac080104@gmail.com>
2023-11-13 16:14:20 -0600

    Merge de mi_rama con dev

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -99,3 +99,27 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+const MapComponent = () => {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 37.7749,
+    longitude: -122.4194,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(newViewport) => setViewport(newViewport)}
+      mapboxApiAccessToken={mapboxgl.accessToken} 
+    />
+  );
+};
+
+export default MapComponent;
