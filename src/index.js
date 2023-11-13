9532e1cfadfaf792fdea76d1671940f085693b13
Jeac04 <jeac080104@gmail.com>
2023-11-13 16:14:20 -0600

    Merge de mi_rama con dev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+import { UserContextProvider } from "./context/contextUser/ContextUser";
 import Initializer from "./routes/initializer/Initializer";
 
-import { BrowserRouter } from "react-router-dom";
-import { UserContextProvider } from "./context/contextUser/ContextUser";
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVhYzA0IiwiYSI6ImNsb3dhYzc3YzB5cjUyb252cmt1bXJnbmkifQ.oC8AUueGT_XpshmXhDJm4A';
 
 ReactDOM.render(
   <UserContextProvider>
