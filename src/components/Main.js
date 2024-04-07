f7e8a4a52f919ef98abf6dca9135d8ae5893bc40
MainLake <leonmaing@gmail.com>
2024-04-07 06:57:04 -0600

    Quitando estilos de componentes Main, Footer y Navbar, Eliminando componente innecesario

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,90 +1,85 @@
-import React from "react";
-import Footer from "./Footer";
+import { Link } from "react-router-dom";
+
+// Importaciones de imaganes
 import Logo from "../imagenes/Logo.png";
-import "../css/main.css";
 import M1 from "../imagenes/mm4.png";
 import M2 from "../imagenes/mm5.png";
 import M3 from "../imagenes/mm6.png";
 
 const Main = () => {
   return (
-    <main className="contenido">
-      <div className="position-relative overflow-hidden p-3 p-md-2 m-md-6 text-center bg-body-tertiary">
-        <div className="col-md-12 p-lg-5 mx-auto my-1 fondo-con-imagen">
-          <h1 className="display-1 fw-bold lh-1 text-white">
-            Perdidos en Tapachula
-          </h1>
-          <div className="d-flex gap-3 justify-content-center lead fw-normal"></div>
-        </div>
-        <div className="product-device shadow-sm d-none d-md-block"></div>
-        <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    <main>
+      
+      <div>
+        <h1>Perdidos en Tapachula</h1>
       </div>
-      <div className="row main-row">
-        <div className="col-lg-4" align="center">
-          <img src={M1} alt="Mascota" className="imagen-circular" />
-          <h2 className="fw-normal">Adopción Responsable</h2>
+
+      <div>
+
+        <div>
+          <img src={M1} alt="Mascota" />
+          <h2>Adopción Responsable</h2>
           <p>
-            <a className="btn btn-secondary" href="/Adopcion-Responsable">
-              Conocer Más &raquo;
-            </a>
+            <Link to="/Adopcion-Responsable">Conocer Más &raquo;</Link>
           </p>
         </div>
-        <div className="col-lg-4" align="center">
-          <img src={M2} alt="Mascota" className="imagen-circular" />
-          <h2 className="fw-normal">¿Por qué tener una Mascota?</h2>
+
+        <div>
+          <img src={M2} alt="Mascota" />
+          <h2>¿Por qué tener una Mascota?</h2>
           <p>
-            <a className="btn btn-secondary" href="/Importancia-Mascotas">
-              Conocer Más &raquo;
-            </a>
+            <Link to="/Importancia-Mascotas">Conocer Más &raquo;</Link>
           </p>
         </div>
-        <div className="col-lg-4" align="center">
-          <img src={M3} alt="Mascota" className="imagen-circular" />
-          <h2 className="fw-normal">¿Cómo Cuido a Mi Mascota?</h2>
+
+        <div>
+          <img src={M3} alt="Mascota" />
+          <h2>¿Cómo Cuido a Mi Mascota?</h2>
           <p>
-            <a className="btn btn-secondary" href="/Cuidados-Mascotas">
-              Conocer Más &raquo;
-            </a>
+            <Link to="Cuidados-Mascotas">Conocer Más &raquo;</Link>
           </p>
         </div>
+
       </div>
-      <div className="container my-4 pet">
-        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-          <div className="col-lg-7 p-3 p-lg-5 pt-lg-3 text-pet-main">
-            <h1 className="display-4 fw-bold lh-1 text-white">
-              ¿Por qué usar Perdidos en Tapachula?
-            </h1>
-            <p className="lead text-white p-4">
-              Perdidos en Tapachula "PET"' ha sido desarrollado con el propósito
-              de brindar asistencia en la búsqueda de mascotas perdidas.
-              Nuestra misión es proporcionar a la comunidad una plataforma que
-              simplifique la tarea de encontrar y reportar mascotas extraviadas.
-              Estamos comprometidos en unir a las mascotas con sus familias y
-              ser un recurso confiable para quienes buscan a sus leales compañeros.
-            </p>
-          </div>
-          <div className="col-lg-5 p-0 overflow-hidden d-flex justify-content-center">
-            <img
-              src={Logo}
-              className="InfoPet"
-              alt="Logo"
-              style={{ maxWidth: "100%", height: "auto" }}
-            />
-          </div>
+
+      <div>
+
+        <div>
+          <h1>¿Por qué usar Perdidos en Tapachula?</h1>
+          <p>
+            Perdidos en Tapachula "PET"' ha sido desarrollado con el propósito
+            de brindar asistencia en la búsqueda de mascotas perdidas.
+            Nuestra misión es proporcionar a la comunidad una plataforma que
+            simplifique la tarea de encontrar y reportar mascotas extraviadas.
+            Estamos comprometidos en unir a las mascotas con sus familias y
+            ser un recurso confiable para quienes buscan a sus leales compañeros.
+          </p>
         </div>
-      </div>
-      <div className="container-fluid subscribe py-1" style={{ backgroundColor: "#6d7d8b" }}>
-      <div className="container text-center py-2">
-        <div className="mx-auto text-center" style={{ maxWidth: "900px" }}>
-          <h1 className="text-white mb-4 display-2 fw-bold">¿Deseas formar parte de Este Proyecto?</h1>
-          <p className="text-white mb-5 lead">Si eres miembro de una asociación o un rescatista independiente comprometido con la salvaguarda de mascotas y deseas unirte a este proyecto, por favor, completa el siguiente formulario. Esto te permitirá formar parte de la sección de Rescatistas y Asociaciones, donde podrás publicar anuncios y campañas relacionadas con tu labor altruista.</p>
-          <div className="position-relative mx-auto">
-            <a className="btn btn-secondary" href="/Rescatista-Form">Ir al Formulario</a>
-          </div>
+
+        <div>
+          <img src={Logo} alt="Logo" />
         </div>
+
       </div>
-    </div>
-      <Footer />
+
+      <div>
+
+        <div>
+
+          <h1>¿Deseas formar parte de Este Proyecto?</h1>
+          
+          <p>
+            Si eres miembro de una asociación o un rescatista independiente comprometido con la salvaguarda de mascotas y deseas unirte a este proyecto, por favor, completa el siguiente formulario. Esto te permitirá formar parte de la sección de Rescatistas y Asociaciones, donde podrás publicar anuncios y campañas relacionadas con tu labor altruista.
+          </p>
+          
+          <div>
+            <Link to="/Rescatista-Form">Ir al Formulario</Link>
+          </div>
+        
+        </div>
+
+      </div>
+
     </main>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,141 +1,99 @@
-import React, { useState } from "react";
-import "../css/navbar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../imagenes/Logo.png";
 
 import { CSpinner } from "@coreui/react";
 
 import { authUserStore } from "../context/globalContext";
-
 import { deleteDataLocalStorage } from "../localstorage/sesionLocalStorage";
 
-const Navbar = () => {
+// Importaciones de imagenes
+import Logo from "../imagenes/Logo.png";
 
+// Importaciones de estilos
+
+const Navbar = () => {
   const { isAuthenticated, logout, user } = authUserStore();
   const [loading, setLoading] = useState(false);
 
   const logoutSession = () => {
+    setLoading(true);
     logout();
     deleteDataLocalStorage();
-  }
+    setLoading(false);
+  };
 
   return (
-    <nav className="navbar navbar-expand-lg crema-bg">
-      <div className="container-fluid">
-        <Link to="/">
-          <img src={Logo} className="style-image" alt="Logo" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+    <nav>
 
-            {
-              isAuthenticated && (
-                <li className="nav-item">
-                  <Link to="/Reportar-Mascotas" className="nav-link">
-                    Reportar Mascotas
-                  </Link>
-                </li>
-              )
-            }
-
-
-            <li className="nav-item">
-              <Link to="/Mascotas-Perdidas" className="nav-link">
-                Mascotas Perdidas
-              </Link>
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
+      
+      <div>
+      
+        <ul>
+      
+          {isAuthenticated && (
+            <li>
+              <Link to="/Reportar-Mascotas">Reportar Mascotas</Link>
             </li>
-            <li className="nav-item">
-              <Link to="/Panel-Rescatistas" className="nav-link">
-                Asociaciones y Rescatistas
-              </Link>
+          )}
+      
+          <li>
+            <Link to="/Mascotas-Perdidas">Mascotas Perdidas</Link>
+          </li>
+      
+          <li>
+            <Link to="/Panel-Rescatistas">Asociaciones y Rescatistas</Link>
+          </li>
+      
+          {isAuthenticated && user.role === "ADMINISTRATOR" && (
+            <li>
+              <Link to="/Admin-Dashboard">Panel de administracion</Link>
             </li>
-
-            {
-              isAuthenticated && user.role === "ADMINISTRATOR" ? (
-                <li className="nav-item">
-                  <Link to="/Admin-Dashboard" className="nav-link">
-                    Panel de administracion
-                  </Link>
-                </li>
-              ) : (null)
-            }
-
-            {
-              isAuthenticated && user.role === "ADMINISTRATOR" ? (
-                <li className="nav-item">
-                  <Link to="/Collaborator-Request" className="nav-link">
-                    Bandeja de peticiones
-                  </Link>
-                </li>
-              ) : (null)
-            }
-
-
-            <li className="nav-item">
-              <Link to="/Como-Reporto" className="nav-link">
-                ¿Cómo Reporto?
-              </Link>
+          )}
+      
+          {isAuthenticated && user.role === "ADMINISTRATOR" && (
+            <li>
+              <Link to="/Collaborator-Request">Bandeja de peticiones</Link>
             </li>
-
-            {
-              isAuthenticated && (
-                <li className="nav-item">
-                  <Link to="/Mis-Mascotas" className="nav-link">
-                    Mis mascotas
-                  </Link>
-                </li>
-              )
-            }
-
-
-
-            {
-              isAuthenticated && user.role === "COLLABORATOR" ? (
-                <li className="nav-item">
-                  <Link to="/Mis-Anuncios" className="nav-link">
-                    Mis Anuncios
-                  </Link>
-                </li>
-              ) : (null)
-            }
-
-
-          </ul>
+          )}
+      
+          <li>
+            <Link to="/Como-Reporto">¿Cómo Reporto?</Link>
+          </li>
+      
+          {isAuthenticated && (
+            <li>
+              <Link to="/Mis-Mascotas">Mis mascotas</Link>
+            </li>
+          )}
+      
+          {isAuthenticated && user.role === "COLLABORATOR" && (
+            <li>
+              <Link to="/Mis-Anuncios">Mis Anuncios</Link>
+            </li>
+          )}
+      
+        </ul>
+      
+        <div>
+      
           {isAuthenticated ? (
-            <div className="d-flex">
-              <a onClick={logoutSession} className="btn btn-danger">
-                {
-                  loading === true ? (
-                    <CSpinner></CSpinner>
-                  ) : (
-                    <>Salir</>
-                  )
-                }
-              </a>
-            </div>
+            <button onClick={logoutSession}>
+              {loading ? <CSpinner /> : "Salir"}
+            </button>
           ) : (
-            <div className="d-flex">
-              <Link to="/Login" className="btn btn-dark me-2">
-                Inicia Sesión
-              </Link>
-              <Link to="/Signup" className="btn btn-dark">
-                Regístrate
-              </Link>
+            <div>
+              <Link to="/Login">Inicia Sesión</Link>
+              <Link to="/Signup">Regístrate</Link>
             </div>
           )}
+      
         </div>
+
       </div>
+    
     </nav>
   );
 };
